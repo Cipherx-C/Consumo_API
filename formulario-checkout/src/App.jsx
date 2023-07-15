d7aa83fcd99548a68e9ebd9ef61bd65ed4476fe8
JovemDevv <YOUR_EMAIL>
2023-07-15 11:12:44 -0300

    Criando o formulario

--- formulario-checkout/src/App.jsx
+++ formulario-checkout/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Lógica para lidar com o envio do formulário
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1 className="text-center">Checkout de Viagem</h1>
+
+      <form id="checkout-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="data-ida" className="form-label">Data de Ida:</label>
+          <input type="date" id="data-ida" className="form-control" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="data-volta" className="form-label">Data de Volta:</label>
+          <input type="date" id="data-volta" className="form-control" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="passageiros-adultos" className="form-label">Passageiros Adultos:</label>
+          <input type="number" id="passageiros-adultos" className="form-control" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="passageiros-criancas" className="form-label">Passageiros Crianças:</label>
+          <input type="number" id="passageiros-criancas" className="form-control" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="origem" className="form-label">Origem:</label>
+          <input type="text" id="origem" className="form-control" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="destino" className="form-label">Destino:</label>
+          <input type="text" id="destino" className="form-control" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="nome-passageiro" className="form-label">Nome do Passageiro Principal:</label>
+          <input type="text" id="nome-passageiro" className="form-control" required />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email-passageiro" className="form-label">E-mail do Passageiro Principal:</label>
+          <input type="email" id="email-passageiro" className="form-control" required />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Enviar</button>
+      </form>
+
+      <div id="mensagem-confirmacao" className="hidden"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
