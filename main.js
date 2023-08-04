a3ea60c01a4745b74bb1ded88da8882dfe2aa9cd
JovemDevv <anacarolinesan18@gmail.com>
2023-08-03 22:43:49 -0300

    inicial

---
+++ main.js
@@ -0,0 +1,25 @@
+const url = "http://localhost:5500/api"
+
+function getUser() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
+    .catch(error => console.error(error))
+}
+
+// GET com paramettro
+function getUsers() {
+    fetch(`${url}/1`)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            userCity.textContent = data.city
+            userAvatar.src = data.avatar
+        })
+        .catch(error => console.error(error))
+}
+
+getUser()
+getUsers()
+
+
